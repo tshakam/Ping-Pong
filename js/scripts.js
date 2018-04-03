572943629b508f090db48daa30e4b9351f5d6b85
tshakam <trevshaka@gmail.com>
2018-04-03 11:57:29 +0300

    edit front end logic to javascript code to delete the previous insertion results

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,32 @@
-// the business logic
-var numbers=[];
-function pingpong(number) {
-  for (var index = 1; index <=number; index++)
-        if (index %15===0) {
-    numbers.push("ping pong");
-    }else if (index % 5===0) {
-      numbers.push("pong");
-    } else if (index %3===0) {
-      numbers.push("ping");
-    }else {
-      numbers.push(index);
-    }
-    }
+function  numberArray(userNumber) {
+  var numbers = [];
+  for (var i = 1; i <= userNumber; i++) {
+    if (( i % 3 === 0) && ( i % 5 === 0)) {
+      numbers.push("pingpong")
+    } else if ( i % 5 === 0) {
+
+      numbers.push("pong")
+
+    } else if ( i % 3 === 0){
+
+      numbers.push("ping")
+
+    } else {
+
+        numbers.push(i)
+
+      }
+  }
+  return numbers;
+}
+
   //front end logic
 $(document).ready(function() {
   $("form#pingpong").submit(function(event) {
     event.preventDefault();
     $("ul").empty();
-    var  number = parseInt($("input#number").val());
-    var result = numberArray(number);
+    var  number1 = parseInt($("input#number1").val());
+    var result = numberArray(number1);
     for (var index = 0; index < result.length; index += 1) {
         $("ul#result").append("<li>" + result[index] + "</li>")
       }
